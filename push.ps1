75a56207a1bea68b887e6322989a0101ca171d58
ww-ops2 <544939100@qq.com>
2026-08-28 11:37:41 +0800

    v5.61 fix category default on edit push.ps1

--- push.ps1
+++ push.ps1
@@ -130,7 +130,7 @@ foreach ($relPath in $filesToPush) {
         $cloudSha = $resp.sha
     } catch {}
 
-    $msg = "v5.60 " + $(if ($cloudSha) { "update" } else { "add" }) + " $relPath"
+    $msg = "v5.61 " + $(if ($cloudSha) { "update" } else { "add" }) + " $relPath"
     $body = @{
         message = $msg
         content = $b64
