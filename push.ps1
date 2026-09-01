8710091fe2a8b3fff1f9278a5219a6951ddb2e7a
ww-ops2 <544939100@qq.com>
2026-09-01 18:51:00 +0800

    v5.73 update push.ps1

--- push.ps1
+++ push.ps1
@@ -130,7 +130,7 @@ foreach ($relPath in $filesToPush) {
         $cloudSha = $resp.sha
     } catch {}
 
-    $msg = "v5.72 " + $(if ($cloudSha) { "update" } else { "add" }) + " $relPath"
+    $msg = "v5.73 " + $(if ($cloudSha) { "update" } else { "add" }) + " $relPath"
     $body = @{
         message = $msg
         content = $b64
